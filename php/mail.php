fd64194813c04aa4bc764338c98f098992fe79bb
Khaled Jallouli <jallouli.khaled@yahoo.fr>
2020-11-08 20:31:10 +0100

    reduce info

--- php/mail.php
+++ php/mail.php
@@ -9,6 +9,6 @@ if(isset($_POST['submit'])){
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$from);
   //  mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-   header('Location:https://khaledjallouli.yj.fr//');
+   header('Location:https://khaledjallouli.github.io/');
     }
 ?>
