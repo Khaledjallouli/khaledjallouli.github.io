ea2e16f81f1bbe89c69909bccd43090c14ea7aeb
Khaled Jallouli <jallouli.khaled@yahoo.fr>
2020-11-08 20:42:03 +0100

    mail

--- php/mail.php
+++ php/mail.php
@@ -1,14 +1,37 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "jallouli.khaled@yahoo.fr"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['name'];
-	$message = $_POST['message'];
-    $subject = $_POST['sujet'];
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$from);
-  //  mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-   header('Location:https://khaledjallouli.github.io/');
-    }
+
+
+
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['sujet'];
+$message = $_POST['message'];
+
+ // $to = 'pk819199@gmail.com';
+ // $subject = 'Website Contactform';
+ // $msg = $name . " " . $email . " " . $message;
+
+ // mail($to, $subject, $msg);
+
+require 'PHPMailerAutoload.php';
+
+
+
+$mail = new PHPMailer();
+
+$mail->isSMTP();
+$mail->Host = "smtp.mail.yahoo.fr";
+$mail->SMTPSecure = "ssl";
+$mail->Port = 465;
+$mail->SMTPAuth = true;
+$mail->Username = $email;
+
+$mail->setFrom('jallouli.khaled@yahoo.fr',$name);
+$mail->addAddress('jallouli.khaled@yahoo.fr');
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+if ($mail->send())
+    echo "Mail sent";
 ?>
